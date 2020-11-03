4394439459354e8961a6e27b1f47ed4c5de6bd0b
SugunaMitrowski <smitrowski@gmail.com>
2020-11-02 22:03:43 -0500

    Lab Part 2

---
+++ Function_Exercise_Circumfrence.js
@@ -0,0 +1,12 @@
+const rad = 4;
+
+
+const circleCircum= circumfrenceCircle(4);
+
+function circumfrenceCircle(r) {
+ const pi = 3.14;
+ 
+ const circumCalc = (2 * pi) * r;
+ return circumCalc;
+
+}
